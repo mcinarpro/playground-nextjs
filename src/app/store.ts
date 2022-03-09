89e00955e45c7cae79166054bbdf9b20cdc46c6f
Mehmet Cinar <mcinarpro@gmail.com>
2022-03-09 23:04:50 +0100

    add redux persist

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,13 +1,28 @@
+import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
 import { Action, configureStore, ThunkAction } from "@reduxjs/toolkit";
 
 import counterReducer from "../features/counter/counterSlice";
 import kanyeReducer from "../features/kanye/kanyeSlice";
 
+const reducers = combineReducers({
+  counter: counterReducer,
+  kanyeQuote: kanyeReducer,
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-    kanyeQuote: kanyeReducer,
-  },
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== "production",
+  //middleware: [thunk],
 });
 
 export type AppDispatch = typeof store.dispatch;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,19 @@
 import { Provider } from "react-redux";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
 
 import { store } from "../app/store";
 
 import type { AppProps } from "next/app";
 
+let persistor = persistStore(store);
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <PersistGate loading={null} persistor={persistor}>
+        <Component {...pageProps} />
+      </PersistGate>
     </Provider>
   );
 }
