de02a24163bd7fb01f68a32cbd391ab6747ff1bf
Mehmet Cinar <mcinarpro@gmail.com>
2022-03-18 04:26:31 +0100

    add @nextui ui lib

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,17 +2,27 @@ import { Provider } from "react-redux";
 import { persistStore } from "redux-persist";
 import { PersistGate } from "redux-persist/integration/react";
 
+import { createTheme, NextUIProvider } from "@nextui-org/react";
+
 import { store } from "../app/store";
 
 import type { AppProps } from "next/app";
-
 let persistor = persistStore(store);
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <Component {...pageProps} />
+        <NextUIProvider
+          theme={createTheme({
+            type: "dark",
+            theme: {
+              colors: {},
+            },
+          })}
+        >
+          <Component {...pageProps} />
+        </NextUIProvider>
       </PersistGate>
     </Provider>
   );
--- src/pages/pokemon.tsx
+++ src/pages/pokemon.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { Card, Grid, Row, Text } from "@nextui-org/react";
 
 interface Pokemon {
   id: number;
@@ -16,5 +16,27 @@ export async function getServerSideProps() {
 }
 
 export default function Pokemon({ pokemon }: { pokemon: Pokemon[] }) {
-  return <div>{JSON.stringify(pokemon)}</div>;
+  return (
+    <Grid.Container gap={2} justify="flex-start">
+      {pokemon.slice(0, 20).map((p) => (
+        <Grid xs={6} sm={3} md={2} xl={1} key={p.id}>
+          <Card
+            hoverable
+            clickable
+            // onClick={onClick}
+          >
+            <Card.Body css={{ p: 1 }}>
+              <Card.Image src={`https://jherr-pokemon.s3.us-west-1.amazonaws.com/${p.image}`} width="100%" height={140} />
+            </Card.Body>
+            <Card.Footer>
+              <Row justify="space-between">
+                <Text transform="capitalize">{p.name}</Text>
+                <Text>#{p.id}</Text>
+              </Row>
+            </Card.Footer>
+          </Card>
+        </Grid>
+      ))}
+    </Grid.Container>
+  );
 }
