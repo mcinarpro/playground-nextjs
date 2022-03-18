a65075d1a4d4a5f7be6c9f05905ca433abbf5303
Mehmet Cinar <mcinarpro@gmail.com>
2022-03-18 22:24:44 +0100

    wip pokemon with useState

--- src/pages/pokemon.tsx
+++ src/pages/pokemon.tsx
@@ -1,4 +1,6 @@
-import { Card, Grid, Row, Text } from "@nextui-org/react";
+import { useMemo, useState } from "react";
+
+import { Card, Grid, Input, Row, Text } from "@nextui-org/react";
 
 interface Pokemon {
   id: number;
@@ -16,27 +18,37 @@ export async function getServerSideProps() {
 }
 
 export default function Pokemon({ pokemon }: { pokemon: Pokemon[] }) {
+  const [filter, setFilter] = useState("");
+
+  const filteredPokemon = useMemo(
+    () => pokemon.filter((pokemon) => pokemon.name.toLowerCase().includes(filter.toLowerCase())),
+    [filter, pokemon]
+  );
+
   return (
-    <Grid.Container gap={2} justify="flex-start">
-      {pokemon.slice(0, 20).map((p) => (
-        <Grid xs={6} sm={3} md={2} xl={1} key={p.id}>
-          <Card
-            hoverable
-            clickable
-            // onClick={onClick}
-          >
-            <Card.Body css={{ p: 1 }}>
-              <Card.Image src={`https://jherr-pokemon.s3.us-west-1.amazonaws.com/${p.image}`} width="100%" height={140} />
-            </Card.Body>
-            <Card.Footer>
-              <Row justify="space-between">
-                <Text transform="capitalize">{p.name}</Text>
-                <Text>#{p.id}</Text>
-              </Row>
-            </Card.Footer>
-          </Card>
-        </Grid>
-      ))}
-    </Grid.Container>
+    <>
+      <Input placeholder="Next UI" value={filter} onChange={(e) => setFilter(e.target.value)} />
+      <Grid.Container gap={2} justify="flex-start">
+        {filteredPokemon.slice(0, 20).map((p) => (
+          <Grid xs={6} sm={3} md={2} xl={1} key={p.id}>
+            <Card
+              hoverable
+              clickable
+              // onClick={onClick}
+            >
+              <Card.Body css={{ p: 1 }}>
+                <Card.Image src={`https://jherr-pokemon.s3.us-west-1.amazonaws.com/${p.image}`} width="100%" height={140} />
+              </Card.Body>
+              <Card.Footer>
+                <Row justify="space-between">
+                  <Text transform="capitalize">{p.name}</Text>
+                  <Text>#{p.id}</Text>
+                </Row>
+              </Card.Footer>
+            </Card>
+          </Grid>
+        ))}
+      </Grid.Container>
+    </>
   );
 }
